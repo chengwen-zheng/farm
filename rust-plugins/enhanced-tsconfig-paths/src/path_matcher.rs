2e2ad7e975f9d27a2a034f44e39872055d7822db
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-09-01 13:57:55 +0800

    feat: format code

--- rust-plugins/enhanced-tsconfig-paths/src/path_matcher.rs
+++ rust-plugins/enhanced-tsconfig-paths/src/path_matcher.rs
@@ -1,72 +1,70 @@
-use std::path::Path;
-use std::collections::HashMap;
 use glob::Pattern;
+use std::collections::HashMap;
+use std::path::Path;
 
 #[derive(Clone)]
 pub struct PathMatcher {
-    base_url: String,
-    paths: HashMap<String, Vec<Pattern>>,
-    main_fields: Vec<String>,
-    match_all: bool,
+  base_url: String,
+  paths: HashMap<String, Vec<Pattern>>,
+  main_fields: Vec<String>,
+  match_all: bool,
 }
 
 impl PathMatcher {
-    pub fn new(
-        base_url: &str,
-        paths: &HashMap<String, Vec<String>>,
-        main_fields: &[String],
-        match_all: bool,
-    ) -> Self {
-        let compiled_paths: HashMap<String, Vec<Pattern>> = paths
-            .iter()
-            .map(|(k, v)| {
-                (
-                    k.clone(),
-                    v.iter()
-                        .map(|p| Pattern::new(p).unwrap())
-                        .collect(),
-                )
-            })
-            .collect();
+  pub fn new(
+    base_url: &str,
+    paths: &HashMap<String, Vec<String>>,
+    main_fields: &[String],
+    match_all: bool,
+  ) -> Self {
+    let compiled_paths: HashMap<String, Vec<Pattern>> = paths
+      .iter()
+      .map(|(k, v)| {
+        (
+          k.clone(),
+          v.iter().map(|p| Pattern::new(p).unwrap()).collect(),
+        )
+      })
+      .collect();
 
-        PathMatcher {
-            base_url: base_url.to_string(),
-            paths: compiled_paths,
-            main_fields: main_fields.to_vec(),
-            match_all,
-        }
+    PathMatcher {
+      base_url: base_url.to_string(),
+      paths: compiled_paths,
+      main_fields: main_fields.to_vec(),
+      match_all,
     }
+  }
 
-    pub fn match_path(&self, request_path: &str, extensions: &[String]) -> Option<String> {
-        println!("Matching path: {}", request_path);
-        for (pattern, substitutions) in &self.paths {
-            let pattern = if self.match_all {
-                format!("{}*", pattern)
-            } else {
-                pattern.clone()
-            };
-            if Pattern::new(&pattern).unwrap().matches(request_path) {
-                for substitution in substitutions {
-                    let potential_path = Path::new(&self.base_url)
-                        .join(substitution.as_str().replace("*", request_path))
-                        .to_str()
-                        .unwrap()
-                        .to_string();
+  pub fn match_path(&self, request_path: &str, extensions: &[String]) -> Option<String> {
+    println!("Matching path: {}", request_path);
+    for (pattern, substitutions) in &self.paths {
+      let pattern = if self.match_all {
+        format!("{}*", pattern)
+      } else {
+        pattern.clone()
+      };
+      if Pattern::new(&pattern).unwrap().matches(request_path) {
+        for substitution in substitutions {
+          let potential_path = Path::new(&self.base_url)
+            .join(substitution.as_str().replace("*", request_path))
+            .to_str()
+            .unwrap()
+            .to_string();
 
-                    if Path::new(&potential_path).exists() {
-                        return Some(potential_path);
-                    }
+          if Path::new(&potential_path).exists() {
+            return Some(potential_path);
+          }
 
-                    for ext in extensions {
-                        let with_extension: String = format!("{}{}", potential_path, ext);
-                        if Path::new(&with_extension).exists() {
-                            return Some(with_extension);
-                        }
-                    }
-                }
+          for ext in extensions {
+            let with_extension: String = format!("{}{}", potential_path, ext);
+            if Path::new(&with_extension).exists() {
+              return Some(with_extension);
             }
+          }
         }
-
-        None
+      }
     }
+
+    None
+  }
 }
--- rust-plugins/enhanced-tsconfig-paths/src/tsconfig_loader.rs
+++ rust-plugins/enhanced-tsconfig-paths/src/tsconfig_loader.rs
@@ -1,8 +1,8 @@
-use std::path::{Path, PathBuf};
-use std::collections::HashMap;
+use anyhow::Result;
 use farmfe_core::serde_json;
 use farmfe_core::serde_json::Value;
-use anyhow::Result;
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
 #[derive(Clone)]
 pub struct TsconfigLoader {
@@ -18,51 +18,60 @@ pub struct TsconfigResult {
 
 impl TsconfigLoader {
   pub fn new() -> Self {
-      TsconfigLoader {
-          visited_dirs: HashMap::new(),
-      }
+    TsconfigLoader {
+      visited_dirs: HashMap::new(),
+    }
   }
 
   pub fn load(&mut self, cwd: &Path) -> Option<TsconfigResult> {
-      let tsconfig_dir = self.get_closest_tsconfig_dir(cwd)?;
-      let tsconfig_path = tsconfig_dir.join("tsconfig.json");
-      
-      let tsconfig_content = std::fs::read_to_string(&tsconfig_path).ok()?;
-      let tsconfig: Value = serde_json::from_str(&tsconfig_content).ok()?;
+    let tsconfig_dir = self.get_closest_tsconfig_dir(cwd)?;
+    let tsconfig_path = tsconfig_dir.join("tsconfig.json");
 
-      let base_url = tsconfig.get("compilerOptions")?.get("baseUrl")?.as_str()?;
-      let paths = tsconfig.get("compilerOptions")?.get("paths")?;
+    let tsconfig_content = std::fs::read_to_string(&tsconfig_path).ok()?;
+    let tsconfig: Value = serde_json::from_str(&tsconfig_content).ok()?;
 
-      let absolute_base_url = tsconfig_dir.join(base_url).to_str()?.to_string();
-      let paths_map: HashMap<String, Vec<String>> = paths
-          .as_object()?
-          .iter()
-          .map(|(k, v)| (k.clone(), v.as_array().unwrap().iter().map(|p| p.as_str().unwrap().to_string()).collect()))
-          .collect();
+    let base_url = tsconfig.get("compilerOptions")?.get("baseUrl")?.as_str()?;
+    let paths = tsconfig.get("compilerOptions")?.get("paths")?;
 
-      Some(TsconfigResult {
-          config_file_abs_path: tsconfig_path.to_str()?.to_string(),
-          absolute_base_url,
-          paths: paths_map,
+    let absolute_base_url = tsconfig_dir.join(base_url).to_str()?.to_string();
+    let paths_map: HashMap<String, Vec<String>> = paths
+      .as_object()?
+      .iter()
+      .map(|(k, v)| {
+        (
+          k.clone(),
+          v.as_array()
+            .unwrap()
+            .iter()
+            .map(|p| p.as_str().unwrap().to_string())
+            .collect(),
+        )
       })
+      .collect();
+
+    Some(TsconfigResult {
+      config_file_abs_path: tsconfig_path.to_str()?.to_string(),
+      absolute_base_url,
+      paths: paths_map,
+    })
   }
 
   fn get_closest_tsconfig_dir(&mut self, cwd: &Path) -> Option<PathBuf> {
-      if cwd.parent() == Some(cwd) {
-          return None;
-      }
+    if cwd.parent() == Some(cwd) {
+      return None;
+    }
 
-      if let Some(&found) = self.visited_dirs.get(cwd) {
-          return if found { Some(cwd.to_path_buf()) } else { None };
-      }
+    if let Some(&found) = self.visited_dirs.get(cwd) {
+      return if found { Some(cwd.to_path_buf()) } else { None };
+    }
 
-      let tsconfig_path = cwd.join("tsconfig.json");
-      if tsconfig_path.exists() {
-          self.visited_dirs.insert(cwd.to_path_buf(), true);
-          Some(cwd.to_path_buf())
-      } else {
-          self.visited_dirs.insert(cwd.to_path_buf(), false);
-          self.get_closest_tsconfig_dir(cwd.parent()?)
-      }
+    let tsconfig_path = cwd.join("tsconfig.json");
+    if tsconfig_path.exists() {
+      self.visited_dirs.insert(cwd.to_path_buf(), true);
+      Some(cwd.to_path_buf())
+    } else {
+      self.visited_dirs.insert(cwd.to_path_buf(), false);
+      self.get_closest_tsconfig_dir(cwd.parent()?)
+    }
   }
 }
